7624fae9e7b039957b215e61de4e2692fb358c58
manikanta995 <72258025+manikanta995@users.noreply.github.com>
2024-01-01 22:18:59 +0530

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+    /*(if -else)
+    
+    {
+    int a , b;
+
+    cout<<"enter the value of a"<< endl;
+    cin>> a;
+
+    cout<<"enter the value of b "<< endl;
+    cin>>b;
+
+    if(a>b){
+      cout<< " a is greater than b" << endl;
+
+    }
+    else{
+      cout <<"b is greater than a "<< endl;
+   
+    }
+}
+*/
+
+/*(if -elseif else)
+{
+int a =24;
+ if(a>20){
+  cout<<"love";
+ }
+else if(a==24){
+cout<<"lovely";
+
+}
+
+else{
+  cout<<"babbar";
+
+}
+
+cout<<a;
+}
+
+/* (while)
+{
+int n;
+cin>>n;
+
+
+int i = 1;
+while(i<=n){
+  cout<<"i"<<" ";
+  i = i + 1;
+ 
+}
+}
+
+*/
+/* sum of no.
+{
+int n;
+cin>>n;
+
+int sum=0;
+ int i = 1;
+while(i<=n){
+  sum=sum+i;
+  i=i+1;
+}
+cout<<"sum of numbers are "<<sum<<endl;
+} 
+
+*/
+
+{
+  int n;
+  cout<<"enter your no";
+  cin>>n;
+  int sum=0;
+  int i = 1;
+while(i<=n){
+  sum=sum+2;
+  i=i+1;
+}
+cout<<"sum of  even numbers are "<<sum<<endl;
+} 
